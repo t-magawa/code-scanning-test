13aaed855a14996b3e25ba67ea63c2ad4d21b21a
maegawa <maegawa@audio-technica.co.jp>
2025-08-01 16:25:14 +0900

    first commit

---
+++ add.c
@@ -0,0 +1,13 @@
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include "add.h"
+#ifdef __cplusplus
+}
+#endif
+
+uint8_t Add(uint8_t a, uint8_t b)
+{
+    return a + b;
+}
---
+++ add.h
@@ -0,0 +1,8 @@
+#ifndef ADD_H
+#define ADD_H
+
+#include <stdint.h>
+
+uint8_t Add(uint8_t a, uint8_t b);
+
+#endif // ADD_H
