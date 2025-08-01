0612b63988c55f50cd1d615737cc127d5da1ddfd
maegawa <maegawa@audio-technica.co.jp>
2025-08-01 16:54:15 +0900

    chore: テスト.

--- add.c
+++ add.c
@@ -9,6 +9,8 @@ extern "C"
 
 const uint8_t test = 0;
 
+uint8_t testArray[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
 uint8_t Add(uint8_t a, uint8_t b)
 {
     return a + b;
@@ -16,5 +18,7 @@ uint8_t Add(uint8_t a, uint8_t b)
 
 uint8_t Test(void)
 {
-    test = 1; // This is a test variable
+    Add(1, 2, 3);
+
+    test = testArray[10];
 }
