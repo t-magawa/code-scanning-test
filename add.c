e07b50f50d2845f2e67ac9f9ef5942173794cafb
maegawa <maegawa@audio-technica.co.jp>
2025-08-01 17:07:43 +0900

    chore: テスト.

--- add.c
+++ add.c
@@ -22,3 +22,19 @@ uint8_t Test(void)
 
     test = testArray[10];
 }
+
+// Function foo's array parameter has a specified size
+void foo(int a[10])
+{
+    int i = 0;
+    for (i = 0; i < 10; i++)
+    {
+        a[i] = i * 2;
+    }
+}
+
+void main()
+{
+    int my_arr[5];
+    foo(my_arr); // my_arr is smaller than foo's array parameter, and will cause access to memory outside its bounds
+}
