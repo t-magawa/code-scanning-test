759e183340b6b6661182c5733152c4c6acfb3a4d
maegawa <maegawa@audio-technica.co.jp>
2025-08-01 16:46:51 +0900

    chore: 静的解析のテストのため誤りのあるコードを追加.

--- add.c
+++ add.c
@@ -7,9 +7,14 @@ extern "C"
 }
 #endif
 
-uint8_t test;
+const uint8_t test = 0;
 
 uint8_t Add(uint8_t a, uint8_t b)
 {
     return a + b;
+}
+
+uint8_t Test(void)
+{
+    test = 1; // This is a test variable
 }
