db099cbf64e13e6b399e97b76892ae75395a5a06
maegawa <maegawa@audio-technica.co.jp>
2025-08-06 17:15:03 +0900

    build: 不要なテスト関数とメイン関数を削除

--- add.c
+++ add.c
@@ -15,26 +15,3 @@ uint8_t Add(uint8_t a, uint8_t b)
 {
     return a + b;
 }
-
-uint8_t Test(void)
-{
-    Add(1, 2, 3);
-
-    test = testArray[10];
-}
-
-// Function foo's array parameter has a specified size
-void foo(int a[10])
-{
-    int i = 0;
-    for (i = 0; i < 10; i++)
-    {
-        a[i] = i * 2;
-    }
-}
-
-void main()
-{
-    int my_arr[5];
-    foo(my_arr); // my_arr is smaller than foo's array parameter, and will cause access to memory outside its bounds
-}
